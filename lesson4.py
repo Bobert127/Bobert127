d5444457243861241732e87a58d04ffc5e4ef649
Robert <roberttrzaska@outlook.com>
2021-06-04 12:57:43 +0200

    third

---
+++ lesson4.py
@@ -0,0 +1,18 @@
+l2 = [125, 126]
+
+
+def phone(number):
+    try:
+        for a in (l2):
+            result = a
+            if a == number:
+                print(number)
+            else:
+                print("nie ma takiego numeru")
+
+    except:
+
+        print("nie ma takiego numeru")
+
+
+phone(127)
