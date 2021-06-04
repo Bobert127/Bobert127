6314a4ad11f8124c184c46c70d37eec0d6e84173
Robert <roberttrzaska@outlook.com>
2021-06-04 20:24:54 +0200

    seventh

---
+++ div.py
@@ -0,0 +1,18 @@
+try:
+    a = int(input("Podaj pierwszą liczbę "))
+    if a != int:
+        print("Podaj liczbę")
+except:
+    print("Podaj liczbę ")
+try:
+    b = int(input("Podaj drugą liczbę "))
+    if a != int or a == 0:
+        print("Podaj liczbę")
+except:
+    print("Podaj liczbę ")
+def div(a, b):
+    return a / b
+
+
+print(a / b)
+
